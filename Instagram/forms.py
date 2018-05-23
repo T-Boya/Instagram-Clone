8c5c4d7be5108b887cf2ed84e4cf9188249907c6
T-Boya <t.osewe1@gmail.com>
2018-05-23 10:42:47 +0300

    added max length

--- Instagram/forms.py
+++ Instagram/forms.py
@@ -56,6 +56,6 @@ class CommentForm(forms.ModelForm):
         fields = ('comment',)
 
 class SearchForm(forms.ModelForm):
-    query = forms.CharField
+    query = forms.CharField(max_length=1000,
         
     
