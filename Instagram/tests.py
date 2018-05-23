2ab61ae66f6d1b30501fe3902f113f5e1e6754a9
T-Boya <t.osewe1@gmail.com>
2018-05-23 10:57:28 +0300

    added tests

--- Instagram/tests.py
+++ Instagram/tests.py
@@ -1,3 +1,37 @@
 from django.test import TestCase
+from .models import Profile, Image, Comment, Like
 
-# Create your tests here.
+
+
+class ProfileTestClass(TestCase):
+
+    def setUp(self):
+        # Setup for test instances
+        self.UserProfile = UserProfile(, bio='I am groot.')
+
+    def test_instance(self):
+        # test to check Profile instance
+        self.assertTrue(isinstance(self.profile, Profile))
+
+    def test_save_profile(self):
+        # test the save method for saving profiles
+        self.UserProfile.save_profile()
+        all_profiles = Profile.objects.all()
+        self.assertTrue(len(all_profiles) > 0)
+
+   
+class PhotoTestClass(TestCase):
+
+    def setUp(self):
+        # Setup for test instances
+        self.Photo = Photo(image='I pooped my pants', title='Last Weekend', description="Don't tell Sam, LOL",)
+
+    def test_instance(self):
+        # test to check Image instance
+        self.assertTrue(isinstance(self.Photo, Image))
+
+    def test_save_image(self):
+        # test to save Image instance
+        self.Photo.save_image()
+        Photos = Photo.objects.all()
+        self.assertTrue(len(Photos) > 0)
